b447285ffd2a9f8fca08c412e77fb28ea16dbc51
John Whiles <johnwhiles@outlook.com>
2017-01-06 12:27:41 +0000

    add database build files

---
+++ src/database-builder/build.js
@@ -0,0 +1,11 @@
+const env = require('env2')('./config.env');
+const fs = require('fs');
+const dbConn = require('../dbconnection.js');
+const sql = fs.readFileSync(`${__dirname}/build_database.sql`).toString();
+
+dbConn.query(sql, (error, result) => {
+  (error
+    ? console.log('Error: ', error)
+    : console.log('Result ', result)
+  );
+});
