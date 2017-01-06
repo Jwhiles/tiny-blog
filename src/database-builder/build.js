2c3ee571d79cf77e203ce77c5aa87e323a9de07a
John Whiles <johnwhiles@outlook.com>
2017-01-06 12:53:52 +0000

    finish database builder

--- src/database-builder/build.js
+++ src/database-builder/build.js
@@ -1,7 +1,7 @@
 const env = require('env2')('./config.env');
 const fs = require('fs');
 const dbConn = require('../dbconnection.js');
-const sql = fs.readFileSync(`${__dirname}/build_database.sql`).toString();
+const sql = fs.readFileSync(`${__dirname}/build.sql`).toString();
 
 dbConn.query(sql, (error, result) => {
   (error
